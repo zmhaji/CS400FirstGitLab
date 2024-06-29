9de5f01810ce3800459942dedeead76300e4a240
ZamZam Haji <zamzam@royal-14.cs.wisc.edu>
2024-06-29 16:54:13 -0500

    completed todo part of change color method

--- Color.java
+++ Color.java
@@ -24,9 +24,11 @@ public class Color{
 	 * 	and different from the exisitng color before making the change
 	 */
 	public boolean  changeColor(String newColor){
+		if(isColorValid(newColor)) {
 		this.color = newColor;
-
 		return true;
+		} 
+		return false;
 	}
 
 	public boolean isColorValid(String inputColor){
